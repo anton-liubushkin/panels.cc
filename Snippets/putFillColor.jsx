aad6687f6bbbb3e2c8bb603cc06be2e2c48e6296
Anton Lyubushkin <nvkz.nemo@gmail.com>
2015-09-19 00:20:20 +0700

    Скрипт раскрашивания шейпов

---
+++ Snippets/putFillColor.jsx
@@ -0,0 +1,20 @@
+/**
+ * Set RGB color for active SOLIDFILL layer
+ * @param {number} r Red color value of 0–255
+ * @param {number} g Green color value of 0–255
+ * @param {number} b Blue color value of 0–255
+ */
+function putFillColor(r, g, b) {  
+    var desc1 = new ActionDescriptor(),  
+        desc2 = new ActionDescriptor(),  
+        desc3 = new ActionDescriptor(),  
+        ref1 = new ActionReference();  
+    ref1.putEnumerated(stringIDToTypeID("contentLayer"), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));  
+    desc1.putReference(charIDToTypeID('null'), ref1);  
+    desc3.putDouble(charIDToTypeID('Rd  '), _r);  
+    desc3.putDouble(charIDToTypeID('Grn '), _g);  
+    desc3.putDouble(charIDToTypeID('Bl  '), _b);  
+    desc2.putObject(charIDToTypeID('Clr '), stringIDToTypeID("RGBColor"), desc3);  
+    desc1.putObject(charIDToTypeID('T   '), stringIDToTypeID("solidColorLayer"), desc2);  
+    executeAction(charIDToTypeID('setd'), desc1, DialogModes.NO);  
+}
